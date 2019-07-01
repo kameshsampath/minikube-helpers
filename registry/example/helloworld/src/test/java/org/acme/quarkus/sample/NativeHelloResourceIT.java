fb4a2de27e153f42ba97b1a5cb90e416112e0949
Kamesh Sampath <kamesh.sampath@hotmail.com>
2019-07-01 15:04:36 +0530

    refactor to simple example

--- registry/example/helloworld/src/test/java/org/acme/quarkus/sample/NativeHelloResourceIT.java
+++
@@ -1,9 +0,0 @@
-package org.acme.quarkus.sample;
-
-import io.quarkus.test.junit.SubstrateTest;
-
-@SubstrateTest
-public class NativeHelloResourceIT extends HelloResourceTest {
-
-    // Execute the same tests but in native mode.
-}
